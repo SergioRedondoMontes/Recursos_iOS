e9eb759ebc5621397636cc137fb9fdea6530103a
Sergio Redondo Montes <redondomontes.sergio@gmail.com>
2020-01-20 18:00:09 +0100

    Added class persistenceData
    La clase PersistenceData es usada junto al coreData

---
+++ recursos_varios/PersistenceService.swift
@@ -0,0 +1,94 @@
+//
+//  PersistenceService.swift
+//
+//  Created by Sergio Redondo on 16/4/18.
+//
+
+import Foundation
+
+import CoreData
+
+class PersistenceService {
+    // MARK: - Core Data stack
+    
+    private init() {}
+    
+    static var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    static var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+         */
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                print("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    static func saveContext () -> Bool {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+                return true
+            } catch {
+                let nserror = error as NSError
+                print("Unresolved error \(nserror), \(nserror.userInfo)")
+                return false
+            }
+        }else{
+            return false
+        }
+    }
+    
+      // MARK: - Core Data Delete Codes
+    
+    static func deleteAllCodesRecords() {
+        let context = persistentContainer.viewContext
+        
+        let deleteFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Codes")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: deleteFetch)
+        
+        do {
+            try context.execute(deleteRequest)
+            try context.save()
+        } catch {
+            print ("There was an error")
+        }
+    }
+    
+    // MARK: - Core Data Delete Security
+
+    
+    static func deleteAllSecurityRecords() {
+        let context = persistentContainer.viewContext
+        
+        let deleteFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Security")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: deleteFetch)
+        
+        do {
+            try context.execute(deleteRequest)
+            try context.save()
+        } catch {
+            print ("There was an error")
+        }
+    }
+}
