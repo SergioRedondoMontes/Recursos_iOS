6acf7b891366031e8a26a94e6dbe437abf5603dd
Sergio Redondo Montes <redondomontes.sergio@gmail.com>
2020-04-19 12:51:56 +0200

    Auto Layout Complete

---
+++ AutoLayout/autoLayout_Completado/autoLayout/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by Sergio Redondo on 19/04/2020.
+//  Copyright © 2020 NBTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let allDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = allDice[Int.random(in: 0...5)]
+        diceImageView2.image = allDice[Int.random(in: 0...5)]
+        
+    }
+
+
+}
+
