a29bc1fbbe6f5bfa68d90d434a5c2fc7b1a735c6
Sergio Redondo Montes <redondomontes.sergio@gmail.com>
2020-04-25 12:31:08 +0200

    Create UICollectionViewCell

---
+++ example_collectionView/example_collectionView/Collection Cell/CVCDemo.swift
@@ -0,0 +1,18 @@
+//
+//  CVCDemo.swift
+//  example_collectionView
+//
+//  Created by Sergio Redondo on 25/04/2020.
+//  Copyright © 2020 NBTech. All rights reserved.
+//
+
+import UIKit
+
+class CVCDemo: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
